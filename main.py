14e759e0687149e652932ef143051632caa7fd3a
tcomlab <tcomlab@gmail.com>
2022-09-08 20:30:24 +0300

    Додано новий функціонал

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 
 04.05.2022 by TC0mLab
 '''
+import tkinter
 
 import ArduinoUsb
 from pystray import MenuItem as item
@@ -11,21 +12,29 @@ from PIL import Image
 import tkinter as tk
 from tkinter import Scale, HORIZONTAL, Button, Label
 
+import json
+
+
+
 
 class light_control:
 
     window = tk.Tk()
-
+    pwm1 = tkinter.IntVar()
+    pwm2 = tkinter.IntVar()
+    pwm3 = tkinter.IntVar()
     # Формуємо та віправляємо пакет по шині USB
     def control(self):
         self.digispark.write(1)
-        self.digispark.write(int(self.w1.get()*2.551))
-        self.digispark.write(int(self.w2.get()*2.551))
-        self.digispark.write(int(self.w3.get()*2.551))
+        self.digispark.write(int(self.pwm1.get() * 2.551))
+        self.digispark.write(int(self.pwm2.get() * 2.551))
+        self.digispark.write(int(self.pwm3.get() * 2.551))
         self.digispark.write(0)
+        self.save_setting()
 
     def quit_window(self, icon, item):
         icon.stop()
+        self.off_light(None,None)
         self.window.destroy()
 
     def show_window(self, icon, item):
@@ -39,6 +48,7 @@ class light_control:
         self.digispark.write(0)
         self.digispark.write(0)
         self.digispark.write(0)
+        self.save_setting()
 
     def withdraw_window(self):
         self.window.withdraw()
@@ -47,6 +57,33 @@ class light_control:
         icon = pystray.Icon("name", image, "title", menu)
         icon.run()
 
+    def load_setting(self):
+        res = ''
+        try:
+            with open('setting.json', 'r') as f:
+                line = f.readline()
+                res = json.loads(line)
+        except FileNotFoundError:
+            setting = {"pwm1": self.pwm1.get(),
+                       "pwm2": self.pwm2.get(),
+                       "pwm3": self.pwm3.get()}
+            ser = json.dumps(setting)
+            with open('setting.json', 'w') as f:
+                f.write(ser)
+                pass
+        self.pwm1.set(res['pwm1'])
+        self.pwm2.set(res['pwm2'])
+        self.pwm3.set(res['pwm3'])
+        self.control()
+
+    def save_setting(self):
+        setting = {"pwm1": self.pwm1.get(),
+                   "pwm2": self.pwm2.get(),
+                   "pwm3": self.pwm3.get()}
+        ser = json.dumps(setting)
+        with open('setting.json', 'w') as f:
+            f.write(ser)
+            pass
     def Start(self):
 
         try:
@@ -64,18 +101,21 @@ class light_control:
 
         self.window.title("Control")
         self.window.geometry("200x280")
-        self.w1 = Scale(self.window, from_=0, to=100, tickinterval=20, orient=HORIZONTAL, length=190, label="Канал 1 Яскравість в %", resolution=5)
-        self.w1.pack()
+        w1 = Scale(self.window, from_=0, to=100, tickinterval=20, orient=HORIZONTAL, length=190,
+                        label="Канал 1 Яскравість в %", resolution=5, variable=self.pwm1)
+        w1.pack()
 
-        self.w2 = Scale(self.window, from_=0, to=100, tickinterval=20, orient=HORIZONTAL, length=190, label="Канал 2 Яскравість в %", resolution=5)
-        self.w2.pack()
+        w2 = Scale(self.window, from_=0, to=100, tickinterval=20, orient=HORIZONTAL, length=190,
+                        label="Канал 2 Яскравість в %", resolution=5, variable=self.pwm2)
+        w2.pack()
 
-        self.w3 = Scale(self.window, from_=0, to=100, tickinterval=100, orient=HORIZONTAL, length=190, label="Канал 3", resolution=100)
-        self.w3.pack()
+        w3 = Scale(self.window, from_=0, to=100, tickinterval=100, orient=HORIZONTAL, length=190,
+                        label="Канал 3", resolution=100, variable=self.pwm3)
+        w3.pack()
 
         btn = Button(self.window, text="Встановити", command=self.control)
         btn.pack()
-
+        self.load_setting()
         self.window.protocol('WM_DELETE_WINDOW', self.withdraw_window)
         self.window.mainloop()
 
